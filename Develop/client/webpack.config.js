9ebffdde0c21ea3a9cea3ab2f92553fea80fff7b
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-29 15:18:44 -0500

    webpack loader rule

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -33,6 +33,19 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+        {
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        }
       ],
     },
   };
